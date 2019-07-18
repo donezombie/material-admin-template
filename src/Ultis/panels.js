0a9ae1524e5c02236622af196aeac0221613a31e
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-07-18 16:45:24 +0700

    Update panels.js

--- src/Ultis/panels.js
+++ src/Ultis/panels.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { Dashboard, Receipt, Store } from '@material-ui/icons';
+import Roles from '../Consts/role';
 import { Authorization } from '../Authentication';
 import Homepage from '../View/HomePage';
 import Order from '../View/Order';
@@ -11,11 +12,12 @@ import StoreView from '../View/Store';
 // subMenu: string - if panel has subMenu prop, Drawer will render by SubMenu List
 
 const panels = (permissionUser, propsRoute) => {
+  const { ACCOUNTANT, SALE, MANAGER } = Roles;
   // Why have "propsRoute"? because in TKDrawer have another Router, it will not use a browserHistory together leads to something wrong when use history push/go Back
   // Authorization(Component, listAllowAccess, listPermissionOfUser)
-  const HomePageWithAuthorization = Authorization(Homepage, ['QL'], permissionUser);
-  const StoreWithAuthorization = Authorization(StoreView, ['QL'], permissionUser);
-  const OrderWithAuthorization = Authorization(Order, ['QL'], permissionUser);
+  const HomePageWithAuthorization = Authorization(Homepage, [MANAGER], permissionUser);
+  const StoreWithAuthorization = Authorization(StoreView, [MANAGER, ACCOUNTANT], permissionUser);
+  const OrderWithAuthorization = Authorization(Order, [MANAGER, ACCOUNTANT], permissionUser);
   return ([
     {
       icon: <Dashboard />,
